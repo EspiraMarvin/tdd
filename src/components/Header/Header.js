f05baee8c3708acefd0f1835724c1c62a1f37ad6
marvin espira <espiramarvin@gmail.com>
2022-09-14 17:37:15 +0300

    feat: updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header({ title }) {
         <>
             <h1 title="Header" className="header">{title}</h1>
             {/* <h3 title="header" className="header">Cats</h3> */}
-            <h3 data-testid="header-1" className="header">Cats</h3>
+            {/* <h3 data-testid="header-1" className="header">Cats</h3> */}
 
         </>
     )
